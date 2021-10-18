dbcdc868fa9e1f153053fa4b54eb806a31f9f545
imranyahya69 <imranyahyaofficial@gmail.com>
2021-10-18 09:23:11 +0500

    title change

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>CV Builder</title>
 
     <link href="{{ asset('css/login_signup.css') }}" rel="stylesheet">
     <link href="{{ asset('css/create.css') }}" rel="stylesheet">
